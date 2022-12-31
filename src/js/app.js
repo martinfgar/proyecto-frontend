9b40982c7bf1b1b1f16407689106fd80dab7714f
martinfgar <martinfifa2@gmail.com>
2022-12-31 16:36:08 +0100

    mierda varia

--- src/js/app.js
+++ src/js/app.js
@@ -8,10 +8,37 @@ function getCookie(name) {
     }
     return null;
 }
-//document.cookie = "access_token = cheese"
+
 
 if (getCookie('access_token') != null){
     document.getElementById('mainPanel').insertAdjacentHTML('beforeend',getCookie('access_token'))
 }else{
     document.getElementById('mainPanel').insertAdjacentHTML('beforeend','patata')
 }
+
+async function logIn(){
+    var data = new FormData(document.getElementById('loginForm'))
+    const res = await fetch('http://localhost:8000/api/login',{
+        method: 'POST',
+        body: data
+    })
+    return res.json()
+}
+
+
+
+document.getElementById('loginForm').addEventListener('submit',async function(event){
+    event.preventDefault()
+    document.getElementById('loginAlert').classList.add('d-none')
+    const res = await logIn()
+    if (res.access_token != undefined){
+        document.cookie = `access_token = ${res.access_token}; max-age=15`
+    }else{
+        document.getElementById('loginAlert').classList.remove('d-none')
+        document.getElementById('loginAlert').innerText = res.message
+    }
+})
+
+chrome.cookies.onChanged.addListener((changedCookie) => {
+    console.log(JSON.stringify(changedCookie.cookie))
+})
